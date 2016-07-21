cf23888c38fb7e2458be1a54e3fff8476dedcd5b
David Blood <davidbjourno@users.noreply.github.com>
2016-07-21 10:51:32 +0100

    Version published 2016-07-21

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -3,22 +3,22 @@ import ngRoute from 'angular-route';
 import ngSanitize from 'angular-sanitize';
 import $ from 'jquery';
 
+const angular = window.angular;
+
 // App module
 const app = angular.module('quizApp', [
   'ngAnimate',
   'ngRoute',
   'ngSanitize',
-  'angular-animation-counter',
-  '720kb.socialshare'
+  '720kb.socialshare',
+  'd3'
 ]);
 
 // Routes
-app.config(['$locationProvider',
-  function ($locationProvider) {
-    // Use the HTML5 history API
-    $locationProvider.html5Mode(true);
-  }
-]);
+app.config(['$locationProvider', $locationProvider => {
+  // Use the HTML5 history API
+  $locationProvider.html5Mode(true);
+}]);
 
 // Controllers
 app.controller('QuizCtrl', ['$scope', '$http',
@@ -28,10 +28,7 @@ app.controller('QuizCtrl', ['$scope', '$http',
     $scope.questionsAnswered = { value: 0 };
     $scope.userScore = { value: 0 };
     $scope.quizStatus = { isOver: false };
-    $scope.remainPercentage = { value: 0 };
-    $scope.leavePercentage = { value: 0 };
-    $scope.result = { text: '' };
-    $scope.message = { content: '' };
+    $scope.message = { text: '' };
 
     angular.forEach(window.quiz.data, row => {
       $scope.questions.push(row);
@@ -44,20 +41,32 @@ app.controller('QuestionCtrl', ['$scope', '$timeout', '$window',
   function ($scope, $timeout, $window) {
     $scope.submitAnswer = function () {
       $scope.answerSubmitted = true;
-      $scope.questionsAnswered.value++;
       $scope.userAnswer = this.choice;
-      $scope.remainAnswer = $scope.questions[$scope.questionsAnswered.value - 1].remainanswer;
-      $scope.leaveAnswer = $scope.questions[$scope.questionsAnswered.value - 1].leaveanswer;
+      $scope.correctAnswer = $scope.questions[$scope.questionsAnswered.value].answer;
 
-      // Check to see if userAnswer matches remainAnswer or leaveAnswer
-      if ($scope.userAnswer === $scope.remainAnswer) {
-        $scope.isRemain = true;
+      // If this is the first question, log a start in GA
+      if ($scope.questionsAnswered.value === 0) {
+        $window.ga('send', 'event', 'Starts', 'Quiz Started', 'Quiz Started');
+      }
+
+      // Check to see if userAnswer is correct
+      if ($scope.userAnswer === $scope.correctAnswer) {
+        $scope.isCorrect = true;
         $scope.userScore.value++;
+
+        // Log correct answer in GA
+        $window.ga('send', 'event',
+          ($scope.questionsAnswered.value < 9 ? '0' : null) +
+          ($scope.questionsAnswered.value + 1) + '. ' +
+          $scope.questions[$scope.questionsAnswered.value].question,
+          'Answer Submitted', $scope.userAnswer + '*');
       } else {
-        if ($scope.userAnswer === $scope.leaveAnswer) {
-          $scope.isLeave = true;
-          $scope.userScore.value--;
-        }
+        // Log incorrect answer in GA
+        $window.ga('send', 'event',
+          ($scope.questionsAnswered.value < 9 ? '0' : null) +
+          ($scope.questionsAnswered.value + 1) + '. ' +
+          $scope.questions[$scope.questionsAnswered.value].question,
+          'Answer Submitted', $scope.userAnswer);
       }
 
       // Display result splash
@@ -68,55 +77,25 @@ app.controller('QuestionCtrl', ['$scope', '$timeout', '$window',
         }, 250);
       }
 
-      function calculatePercentage() {
-        // Calculate user score as a percentage
-        $scope.remainPercentage.value = Math.round($scope.userScore.value /
-          $scope.questions.length * 50);
-
-        // If userPercentage < 0, make it into leavePercentage
-        if ($scope.remainPercentage.value <= 0) {
-          if ($scope.remainPercentage.value < 0) {
-            $scope.leavePercentage.value = -$scope.remainPercentage.value;
-          } else {
-            console.log('User is ambivalent');
-          }
-        }
-
-        console.log('remainPercentage: ' + $scope.remainPercentage.value,
-        'leavePercentage: ' + $scope.leavePercentage.value);
-      }
-
       function message() {
-        if ($scope.remainPercentage.value <= 0) {
-          if ($scope.remainPercentage.value < 0) {
-            $scope.message.content = 'If you aren\'t already, you may want to' +
-              ' consider voting <strong>leave</strong>.';
+        if ($scope.userScore.value > $scope.questions.length / 2) {
+          if ($scope.userScore.value === $scope.questions.length) {
+            $scope.message.text = 'first rate!';
           } else {
-            $scope.message.content = 'Still undecided? Go to our ' +
-            "<a href='http://www.ft.com/eu-referendum'>EU referendum home page</a> for all the FT's referendum coverage.";
+            $scope.message.text = 'not too shabby!';
           }
         } else {
-          $scope.message.content = 'If you aren\'t already, you may want to' +
-            ' consider voting <strong>remain</strong>.';
+          $scope.message.text = 'room for improvement!';
         }
       }
 
       // Check to see if quiz is over
-      if ($scope.questionsAnswered.value === $scope.questions.length) {
+      if ($scope.questionsAnswered.value === $scope.questions.length - 1) {
+        $scope.questionsAnswered.value++;
         $scope.quizStatus.isOver = true;
-
-        calculatePercentage();
         message();
-
-        if ($scope.remainPercentage.value <= 0) {
-          if ($scope.remainPercentage.value < 0) {
-            $scope.result.text = 'leave';
-          } else {
-            $scope.result.text = 'Completely neutral';
-          }
-        } else {
-          $scope.result.text = 'remain';
-        }
+      } else {
+        $scope.questionsAnswered.value++;
       }
 
       // Update progress bar
@@ -127,3 +106,107 @@ app.controller('QuestionCtrl', ['$scope', '$timeout', '$window',
     };
   }
 ]);
+
+// Services
+angular.module('d3', []).factory('d3Service', [function () {
+  const d3 = window.d3;
+  return d3;
+}]);
+
+// Directives
+/*eslint-disable*/
+app.directive('d3ArcTween', ['d3Service', function (d3Service) {
+  return {
+    restrict: 'EA',
+    scope: true,
+    link: function ($scope, $element) {
+      var width = 200; // Max width of grid column
+      var height = 200; // outerRadius * 2
+      var τ = 2 * Math.PI; // http://tauday.com/tau-manifesto
+
+      // An arc function with all values bound except the endAngle. So, to compute an
+      // SVG path string for a given angle, we pass an object with an endAngle
+      // property to the `arc` function, and it will return the corresponding string.
+      var arc = d3.svg.arc()
+          .innerRadius(80)
+          .outerRadius(100)
+          .startAngle(0);
+
+      // Create the SVG container, and apply a transform such that the origin is the
+      // center of the canvas. This way, we don't need to position arcs individually.
+      var svg = d3.select($element[0]).append('svg')
+          .attr('width', width)
+          .attr('height', height)
+          .style('position', 'relative')
+          .style('top', '-80px')
+        .append('g')
+          .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+      // Add the background arc, from 0 to 100% (τ).
+      var background = svg.append('path')
+          .datum({endAngle: τ})
+          .style('fill', '#a7a59b')
+          .attr('d', arc);
+
+      // Add the foreground arc in orange, currently showing 12.7%.
+      var foreground = svg.append('path')
+          .datum({endAngle: 0})
+          .style('fill', '#458b00')
+          .attr('d', arc);
+
+      // Every so often, start a transition to a new random angle. Use transition.call
+      // (identical to selection.call) so that we can encapsulate the logic for
+      // tweening the arc in a separate function below.
+      var interval = setInterval(function () {
+        foreground.transition()
+            .duration(1000)
+            .call(arcTween, $scope.userScore.value / $scope.questions.length * τ);
+      }, 1000);
+
+      // Creates a tween on the specified transition's "d" attribute, transitioning
+      // any selected arcs from their current angle to the specified new angle.
+      function arcTween(transition, newAngle) {
+        // The function passed to attrTween is invoked for each selected element when
+        // the transition starts, and for each element returns the interpolator to use
+        // over the course of transition. This function is thus responsible for
+        // determining the starting angle of the transition (which is pulled from the
+        // element's bound datum, d.endAngle), and the ending angle (simply the
+        // newAngle argument to the enclosing function).
+        transition.attrTween('d', function (d) {
+          // To interpolate between the two angles, we use the default d3.interpolate.
+          // (Internally, this maps to d3.interpolateNumber, since both of the
+          // arguments to d3.interpolate are numbers.) The returned function takes a
+          // single argument t and returns a number between the starting angle and the
+          // ending angle. When t = 0, it returns d.endAngle; when t = 1, it returns
+          // newAngle; and for 0 < t < 1 it returns an angle in-between.
+          var interpolate = d3.interpolate(d.endAngle, newAngle);
+          // The return value of the attrTween is also a function: the function that
+          // we want to run for each tick of the transition. Because we used
+          // attrTween("d"), the return value of this last function will be set to the
+          // "d" attribute at every tick. (It's also possible to use transition.tween
+          // to run arbitrary code for every tick, say if you want to set multiple
+          // attributes from a single function.) The argument t ranges from 0, at the
+          // start of the transition, to 1, at the end.
+          return function (t) {
+            // Calculate the current arc angle based on the transition time, t. Since
+            // the t for the transition and the t for the interpolate both range from
+            // 0 to 1, we can pass t directly to the interpolator.
+            //
+            // Note that the interpolated angle is written into the element's bound
+            // data object! This is important: it means that if the transition were
+            // interrupted, the data bound to the element would still be consistent
+            // with its appearance. Whenever we start a new arc transition, the
+            // correct starting angle can be inferred from the data.
+            d.endAngle = interpolate(t);
+            // Lastly, compute the arc path given the updated data! In effect, this
+            // transition uses data-space interpolation: the data is interpolated
+            // (that is, the end angle) rather than the path string itself.
+            // Interpolating the angles in polar coordinates, rather than the raw path
+            // string, produces valid intermediate arcs during the transition.
+            return arc(d);
+          };
+        });
+      }
+    }
+  };
+}]);
